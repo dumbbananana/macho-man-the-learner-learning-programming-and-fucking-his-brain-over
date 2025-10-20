25aea86d2193d2779f17c0da2c263e6de86d0863
ashish <atimsina2021@gmail.com>
2025-10-20 18:18:47 +0545

    h

--- brainfuck.js
+++ brainfuck.js
@@ -5,7 +5,7 @@ document.getElementById("button").onclick = function() {
    userinput = document.getElementById("input1").value ;
    passinput = document.getElementById("input2").value ;
 
-   if (username == userinput && password == passinput){
+   if (username == userinput && 18 <= passinput){
      window.alert("correct work ma nigga")
    } else{
     window.alert("you the dumbest nigga i know")
