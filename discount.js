e6a9e159a676f51a656603cbfcb6c36c3b143489
ashish <atimsina2021@gmail.com>
2025-10-23 00:31:00 +0545

    Fix logic in discount.js: remove erroneous condition and ensure proper user authentication flow

--- discount.js
+++ discount.js
@@ -1,3 +1,18 @@
-if(1 || 0 && 1) {
-    );
-}
+const password = abc;
+const user = abc;
+
+ ask = prompt("whose here ?" , "");
+ if (!(ask == user)){
+    alert("i dont know you");
+ } else if (ask == ""){
+  alert("cancelled");
+ } else{
+    ask2=prompt("whats the password" , "");
+     if (!(ask2== password) ){
+       alert("wrong password");
+     } else if(!(ask2== "")){
+        alert("welcome");
+     } else{
+        alert("cancelled");
+     }
+ };
