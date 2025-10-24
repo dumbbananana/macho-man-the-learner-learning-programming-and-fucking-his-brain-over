95afe1f1dce6f3a178470bfe8ad41ffb0a567c4c
ashish <atimsina2021@gmail.com>
2025-10-24 12:24:31 +0545

    ac

--- discount.js
+++ discount.js
@@ -1,7 +1,8 @@
 const discount = 10;
 const cost = 10;
-let play = false ; 
-
+ let play = false ; 
+ 
+document.getElementById("press").onclick  = function(){
 while(!play){
 let  quantity  = document.getElementById("quantity").value;
   
@@ -9,7 +10,11 @@ let purchased = false;
 
 while(!purchased){
     if(isNaN(quantity)){
-        alert("please enter a number");
+        alert("please enter a number")
+        
+        purchased = true;
+       play = true ;
+    
     } else if(quantity == null) {
         alert("cancelled");
         break;
@@ -23,13 +28,15 @@ while(!purchased){
 
         if(quantity >= 100){
             
-            alert(`you get a discount of 10 which makes your total number ${(quantity * cost)* discount/100}`);
-            purchased = true;}
+            alert(`you get a discount of 10 which makes your total number ${(quantity * cost)* discount/100}`)
+            purchased = true
+            play = true;}
             else if(quantity < 1 ){
                 alert("so youre gonna borrow from me or what");
             } else{
                 alert(`you purchased ${quantity} for the total price  of ${quantity * cost}`);
                 purchased = true;
+                play = true;
             }
 
             if(!true){
@@ -37,10 +44,12 @@ while(!purchased){
              if(doyou == "yes"){
                 purchased = false;
                 alert("thanks for playing");
+             } else{
+                play = true;
              }
             }
             }
-        
 }
- 
-}
+
+        }        
+    }
