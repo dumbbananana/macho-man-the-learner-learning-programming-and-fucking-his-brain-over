074608176f715a1954f94cc4756f91055f048819
ashish <atimsina2021@gmail.com>
2025-10-24 16:44:43 +0545

    dome much gg

--- discount.js
+++ discount.js
@@ -1,25 +1,12 @@
+let username=prompt("enter your username", "");
+yesno=prompt("do you want to make it more profelssional", "");
+if(yesno == "yes"){
+     usernamet=username.trim().slice(0,1).toUpperCase() + username.slice(1).toLowerCase();
 
-let running = true;
-while(running){
-let fullname = prompt("what is your full name" , "");
+     alert(usernamet)
+    alert(`your username is ${username}`);
+} 
 
-
-let firstname = fullname.slice(0 , fullname.indexOf(" "));
-let lastname = fullname.slice(fullname.indexOf(' ')); 
-
-
-alert(`your full name is ${firstname} and your last name is ${lastname}`);
-if(fullname.length > 15){
-    alert("wow thats a rather long name");
-}
-
-if(fullname.length < 5){
-    alert("thats a rather short name you have there");
-}
-doyou=prompt("do you want ot play again ?"  , "");
-if(doyou=="yes"){
-    continue;
-} else{
-    running = false;
-}
+  else{
+      alert(`your username is ${username}`);
 }
