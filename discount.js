099ebe7ba36a194b25151f13532f506e1bd39f79
ashish <atimsina2021@gmail.com>
2025-10-24 16:20:00 +0545

    hello potato wolrd

--- discount.js
+++ discount.js
@@ -9,6 +9,13 @@ let lastname = fullname.slice(fullname.indexOf(' '));
 
 
 alert(`your full name is ${firstname} and your last name is ${lastname}`);
+if(fullname.length > 15){
+    alert("wow thats a rather long name");
+}
+
+if(fullname.length < 5){
+    alert("thats a rather short name you have there");
+}
 doyou=prompt("do you want ot play again ?"  , "");
 if(doyou=="yes"){
     continue;
