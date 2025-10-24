3aa307f8d453f5dbb67414fc8ec206bdccf93c4e
ashish <atimsina2021@gmail.com>
2025-10-24 14:01:15 +0545

    asf

--- discount.js
+++ discount.js
@@ -1,55 +1,8 @@
-const discount = 10;
-const cost = 10;
- let play = false ; 
- 
-document.getElementById("press").onclick  = function(){
-while(!play){
-let  quantity  = document.getElementById("quantity").value;
-  
-let purchased = false;
+let fullname = prompt("what is your full name" , "");
 
-while(!purchased){
-    if(isNaN(quantity)){
-        alert("please enter a number")
-        
-        purchased = true;
-       play = true ;
-    
-    } else if(quantity == null) {
-        alert("cancelled");
-        break;
-        play = true;
-    } else if(quantity == ""){
-        alert("emppty");
-    }  
-    
-    else{
-     quantity = Number(quantity);
 
-        if(quantity >= 100){
-            
-            alert(`you get a discount of 10 which makes your total number ${(quantity * cost)* discount/100}`)
-            purchased = true
-            play = true;}
-            else if(quantity < 1 ){
-                alert("so youre gonna borrow from me or what");
-            } else{
-                alert(`you purchased ${quantity} for the total price  of ${quantity * cost}`);
-                purchased = true;
-                play = true;
-            }
+let firstname = fullname.slice(0 , fullname.indexOf(" "));
+let lastname = fullname.slice(fullname.indexOf(' ')); 
 
-            if(!true){
-                doyou=prompt("do you still want to purchase ?" , "");
-             if(doyou == "yes"){
-                purchased = false;
-                alert("thanks for playing");
-             } else{
-                play = true;
-             }
-            }
-            }
-}
 
-        }        
-    }
+alert(`your full name is ${firstname} and your last name is ${lastname}`);
