a0261d6e08ceda905003b993d5b65e42af2f1f8f
ashish <atimsina2021@gmail.com>
2025-10-24 09:41:22 +0545

    sui

--- discount.js
+++ discount.js
@@ -2,3 +2,9 @@ let a=Number(prompt("what is the value of a", ""));
 
 let b=Number(prompt("what is the value of b", ""));
 
+function addition( x , y){
+   alert( x + y )
+  
+}
+
+addition( a , b);
