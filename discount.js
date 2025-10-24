b42d8bb4876cb098873324649432c5b4c3577567
ashish <atimsina2021@gmail.com>
2025-10-24 15:42:30 +0545

    lesgo

--- discount.js
+++ discount.js
@@ -1,3 +1,6 @@
+
+let running = true;
+while(running){
 let fullname = prompt("what is your full name" , "");
 
 
@@ -6,3 +9,10 @@ let lastname = fullname.slice(fullname.indexOf(' '));
 
 
 alert(`your full name is ${firstname} and your last name is ${lastname}`);
+doyou=prompt("do you want ot play again ?"  , "");
+if(doyou=="yes"){
+    continue;
+} else{
+    running = false;
+}
+}
