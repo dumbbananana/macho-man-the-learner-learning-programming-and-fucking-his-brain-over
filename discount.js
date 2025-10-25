918aad343cb3b19938a039e6ca999fab6e33e747
ashish <atimsina2021@gmail.com>
2025-10-25 20:34:34 +0545

    still learnign

--- discount.js
+++ discount.js
@@ -1,35 +1,32 @@
-let given=document.getElementById("giventemp") ;
-let checkifc=document.getElementById("checkboxic");
-let checkiff=document.getElementById("checkboxif");
-let result=document.getElementById("result");
-let temp;
+let input = document.getElementById("giventemp");
+let cchecked = document.getElementById("checkboxic");
+let fchecked = document.getElementById("checkboxif");
+let answer = document.getElementById("result");
+let temp ;
 
+document.getElementById("button").onclick = function(){
+   
 
-document.getElementById("button").onclick = function (){
+   
   
-temp = Number(given.value);
+    if(fchecked.checked)
+    {
+        temp = Number(input.value);
+         temp = temp * 9 / 5 + 32 ;
+        answer.value = temp.toFixed(1) + "˚f";
+        console.log(temp , answer)
+        
+    } 
+    else if(cchecked.checked)
+    {
+        temp = Number(input.value);
+        temp = (temp-32)*5/9 ;
+        
+        answer.value = temp.toFixed(1) + "˚c";
+        console.log(temp , answer)
+        
+    } 
 
-if(temp == "")
-{
-    alert("please enter a number ")
-
-
-}
-
-else{
+   
   
-if(checkifc.checked)
-{
-    temp = (temp - 32) * (5/9);
-    result.value = temp.toFixed(1) + "˚c";
-
-} 
-
-else if (checkiff.checked)
-{
-    temp = temp  * 9 / 5 + 32;
-    result.value = temp.toFixed(1) + "˚f"
-
-} 
-}
 };
