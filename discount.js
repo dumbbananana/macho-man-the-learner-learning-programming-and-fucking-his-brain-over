8c98d3d9ca7c38eda9711c6a22e9f25f41c21a12
ashish <atimsina2021@gmail.com>
2025-10-26 14:42:21 +0545

    supermarket applicaiane

--- discount.js
+++ discount.js
@@ -1,9 +1,15 @@
- let a = true ;
- while(a){
-     let ask = prompt("what is your name" ,"");
-      if(ask == ""){
-        alert("no input ")
-        a = false ;
-      }
+let rows = 
+[ 
+["apple" , "banana" , "mango"] ,
+["suppa" , "duppa" , "muppa"] ,
+["bananapie" , "smottie", " butterchicken"]
+];
 
- }
+let ask = prompt("what do you want ?" ,"");
+
+for( let i = 0; i<rows.length; i++){
+   let ii = rows[i].indexOf(`${ask}`);
+   if(ii !== -1){
+    alert(`${ask} is in ${i + 1} row`);
+   }
+}
