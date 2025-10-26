16931e5bf69840221c9ed795efdcc5a3d7c20bed
ashish <atimsina2021@gmail.com>
2025-10-26 20:55:59 +0545

    wehehe

--- discount.js
+++ discount.js
@@ -1,28 +1,49 @@
 let rows = 
 [
-    ["a", "b"],
-    ["c", "d"],
-    ["e", "f"]
+    ["apple", "banana"],
+    ["candy", "lolly"],
+    ["egg", "fish"]
     
 ];
-//
+
+let a = true;
+
+
 
 let ask = prompt("what do you want ", "");
 for(let i = 0; i < rows.length; i++)
 {
+   
+
   
    let ii = rows[i].indexOf(`${ask}`);
 
    if(ii != -1){
-    i = ii +1;
+    i = i +1;
     ii = ii +1;
-    alert(`${ask} is found in ${ii} columb if ${i}`)
+    alert(`${ask} is found in ${ii} columb if ${i} row`);
+    console.log(i);
+    console.log(ii);
     break;
-   } else if ( ask == null){
-    alert("cancelled")
+   } 
+   
+    if ( ask == null){
+    alert("cancelled");
+    console.log(i);
+    console.log(ii);
+    a = false;
     break;
-   } else {
-    alert("you value is not found")
-   }
+   } 
+   
+   else if(ask == "") {
+    console.log(i);
+    console.log(ii);
+    alert("empty");
+    break;
+   } 
 }
+   if(a = false){
+    prompt("do you want to check again")
+ 
 
+}
