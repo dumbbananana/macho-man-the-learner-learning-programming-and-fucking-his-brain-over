99dcc782da6cd5c6f494403eb02f8fcbd50b4aa3
ashish <atimsina2021@gmail.com>
2025-10-26 15:38:36 +0545

    still  dung

--- discount.js
+++ discount.js
@@ -5,11 +5,39 @@ let rows =
 ["bananapie" , "smottie", " butterchicken"]
 ];
 
-let ask = prompt("what do you want ?" ,"");
 
+ let a = true;
+let b = true;
+
+while(a){
+    let ask;
+     ask = prompt("what do you want ?" ,"");
+
+while(b){
+
+    
 for( let i = 0; i<rows.length; i++){
-   let ii = rows[i].indexOf(`${ask}`);
+
+  let ii ;
+    ii = rows[i].indexOf(`${ask}`);
    if(ii !== -1){
-    alert(`${ask} is in ${i + 1} row`);
+    alert(`${ask} is in ${i + 1}  row and ${ii+1} columb`);
+     an=prompt("do you need anythign else ?", "");
+     if( an =="yes"){
+        break;
+
+     } else{
+        alert("exited");
+        a= false;
+        break;
+    
+     }
+   } else{
+    alert("item not found");
+    b = false;
+     break;
+
    }
 }
+}
+};
