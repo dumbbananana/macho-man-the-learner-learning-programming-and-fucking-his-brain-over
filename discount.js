a14fff9410e28fb1f48f93361c710e65b0b59bbb
ashish <atimsina2021@gmail.com>
2025-10-26 12:07:40 +0545

    going down not good i for got what i learned

--- discount.js
+++ discount.js
@@ -1,32 +1,9 @@
-let input = document.getElementById("giventemp");
-let cchecked = document.getElementById("checkboxic");
-let fchecked = document.getElementById("checkboxif");
-let answer = document.getElementById("result");
-let temp ;
+ let a = true ;
+ while(a){
+     let ask = prompt("what is your name" ,"");
+      if(ask == ""){
+        alert("no input ")
+        a = false ;
+      }
 
-document.getElementById("button").onclick = function(){
-   
-
-   
-  
-    if(fchecked.checked)
-    {
-        temp = Number(input.value);
-         temp = temp * 9 / 5 + 32 ;
-        answer.value = temp.toFixed(1) + "˚f";
-        console.log(temp , answer)
-        
-    } 
-    else if(cchecked.checked)
-    {
-        temp = Number(input.value);
-        temp = (temp-32)*5/9 ;
-        
-        answer.value = temp.toFixed(1) + "˚c";
-        console.log(temp , answer)
-        
-    } 
-
-   
-  
-};
+ }
