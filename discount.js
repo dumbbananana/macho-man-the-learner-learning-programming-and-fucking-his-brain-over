c15a95aeee209635b5ba2309fb261e669b12e4e6
ashish <atimsina2021@gmail.com>
2025-10-27 16:18:27 +0545

    learning still

--- discount.js
+++ discount.js
@@ -1,55 +1,17 @@
-let rows = 
-[
-    ["apple", "banana"],
-    ["candy", "lolly"],
-    ["egg", "fish"]
+let a = prompt("what do you want ?" , "");
+
+ a = ["10000" , "20000" , "30000" , "40000"];
+
+let max = Math.max(...a);
+let min = Math.min(...a);
+alert(` the highest price for this item isn ${max} rs`);
+alert(` the lowest price for this item isn ${min} rs`);
+
+let sum = 0;
+for(let i = 0; i <a.length ; i++){
+    sum = sum + Number(a[i]);
     
-];
-
-
-
-
-let a = true;
-while(a){
-    let b = true;
-let ask = prompt("what do you want ", "");
-while(b){
-//let ask = prompt("what do you want ", "");
-for(let i = 0; i < rows.length; i++)
-{
-   
-
-  
-   let ii = rows[i].indexOf(`${ask}`);
-
-   if(ii != -1){
-    i = i +1;
-    ii = ii +1;
-    alert(`${ask} is found in ${ii} columb if ${i} row`);
-    console.log(i);
-    console.log(ii);
-    b = false;
-     a = false;
-   }  
-   
-    if ( ask == null){
-    alert("cancelled");
-    console.log(i);
-    console.log(ii);
-    a = false;
-    break;
-   } 
-   
-   else if(ask == "") {
-    console.log(i);
-    console.log(ii);
-    alert("empty");
-    break;
-   } else if( ii == -1){
-    alert("not found");
-    a = false;
-    
-   }
-}
-}  
-}
+};
+alert(` the total market value of bid is ${sum} rs`)
+let average = sum / a.length;
+alert(` average amount  = ${average} rs`)
