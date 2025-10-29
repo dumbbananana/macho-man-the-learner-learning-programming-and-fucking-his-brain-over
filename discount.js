528e225b406126270dab57c0fb6d0b9c194a03d9
ashish <atimsina2021@gmail.com>
2025-10-29 18:15:59 +0545

    learning still

--- discount.js
+++ discount.js
@@ -1,69 +1,20 @@
-let inn = prompt("do you want to include number ?" ,"");
-if( inn == "yes"){
-     inn = true;
-}else{
-    inn = false;
+while( ask == "done"){
+
+    let ask = prompt("what do you want", "");
+    if(ask == null){
+        alert("item entered");
+        break;
+    } else {
+        ask = ask;
+    }
+    let ask1 = prompt("how many do you want ?", "");
+     if(ask1 == null){
+        alert("no digit enterrred");
+        break;
+    } else {
+        ask1 = ask1;
+    }
 }
 
-let il = prompt("do you want to include letters ?" ,"");
-if( il == "yes"){
-     il = true;
-}else{
-    il = false;
-}
-
-let is = prompt("do you want to include letters ?" ,"");
-if( is == "yes"){
-     is = true;
-}else{
-    is = false;
-}
-
-let leng = prompt("what is the length of your password ?" ,"");
-if(leng <5 ){
-    alert("numbers less tahn 5 is not allowed");
-} else {
-
-let includenums = inn;
-let includeletters = il;
-let includesymbols = is;
-let alength = leng;
-let password = "";
-
-alert(inn)
-alert(il)
-alert(is)
-alert(leng)
-
-function generatepassword(includenums, includeletters,   includesymbols, alength ){
-    
-     let allowednums = "12345";
-     let allowedletters = "abcde";
-      let allowedsymbols = "!@#$%";
-
-        let whatall = ""; 
-        
-      whatall += includenums ? allowednums :  "";
-      whatall += includeletters ? allowedletters :  "";
-      whatall += includesymbols ? allowedsymbols :  "";
-     
-      
-     
-
-      for(let i = 0; i < alength ; i++){
-        let index = Math.floor(Math.random() * whatall.length);
-        password += whatall[index];
-
-       
-      
-      }
-      
-      console.log(password)
-}
-
-
- password = generatepassword(includenums, includeletters, includesymbols, alength );
-
-
-
-}
+console.log(ask)
+console.log(ask1)
