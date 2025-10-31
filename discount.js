d0664306bf8e0aeda63b8fcdd31d790dfb3d78b1
ashish <atimsina2021@gmail.com>
2025-11-01 00:40:42 +0545

    small steps at a time

--- discount.js
+++ discount.js
@@ -1,16 +1,34 @@
+let abc  = [];
+let sagarmatha  = [];
+let kanchangunga = [];
+let final;
+
+let given =false;
+while(given == false){
+    let age = prompt("what is your age ", "");
+      age = Number(age)
+    if( age >= 18){
+        let name  = prompt("what is your name ", "");
+        let where = prompt("where do you want to go", "");
+        if ((name || where ) == ""){
+            alert("hellow world")
+
+        }  else{
+
+           if(where == "abc"){
+            let semi = name +`-`+ where ;
+            
+            abc.push(semi)
+            
+            given = true;
+            
+         
+           }
+        }
 
 
-let fruit = prompt("what is your fav food?", "")
-let num = prompt("what is your fav num?", "")
-let word = prompt("what is your fav word?", "")
-
-let id = [fruit , num , word]
-
-console.log(id);
-
-
-id.forEach(call)
-
-function call(element){
-    console.log(element)
+    }
 }
+
+
+console.log(abc)
