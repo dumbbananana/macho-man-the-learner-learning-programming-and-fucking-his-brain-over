a1e7401204485a711c46c4d8e5123de3ca8d9750
ashish <atimsina2021@gmail.com>
2025-11-01 15:38:05 +0545

    something works something dont

--- discount.js
+++ discount.js
@@ -1,6 +1,34 @@
-function hello(data){
-    console.log(`${data} hellow`)
+let password =  [];
+let result = "";
+let suppa = "";
+let supply;
+
+let helo = true;
+while(helo){
+
+
+    let username = prompt("enter a username", "");
+    let password1 = prompt("enter passowrd", "");
+let finalnum = "";
+let symbols = "!@#$%^&";
+let numbers = "123456789";
+finalnum += symbols;
+finalnum += numbers;
+let password11 = password1.length;
+for(i = 0; i <  password11; i ++){
+    
+    result = Math.floor(Math.random() * 10)
+     suppa += finalnum[result];
+     supply = username + `-` + suppa;
+     
+    
+     helo = false;
+}
+console.log(suppa)
+console.log(result)
+password.push(supply)
+console.log(password);
+console.log(password1);
 }
 
-let hellow = 2 + 2;
-hello(hellow)
+
