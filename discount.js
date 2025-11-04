023afeae709e3c41bfecc92c19c35c4fb2a2c707
ashish <atimsina2021@gmail.com>
2025-11-04 09:44:36 +0545

    still a working caluclator

--- discount.js
+++ discount.js
@@ -2,12 +2,40 @@ function  plus(a , b){
     answer = a + b ;
     console.log(answer)
 }
+function  muntiply(a , b){
+    answer = a * b ;
+    console.log(answer)
+}
+function  divide(a , b){
+    answer = a / b ;
+    console.log(answer)
+}
+function  minus(a , b){
+    answer = a - b ;
+    console.log(answer)
+}
 
 let ask = prompt("enter the first number" ,)
 let ask2 = prompt("enter the second number" ,)
+let ask0  = prompt("what do you want to do?  ", );
 
 if(!(ask && ask2) == ""){
     ask  = Number(ask);
     ask2 = Number(ask2);
-   plus(ask , ask2)
+  
+
+   if(ask0 == "add"){
+    plus(ask , ask2)
+   }
+
+    if(ask0 == "minus"){
+    minus(ask , ask2)
+   }
+
+   if(ask0 == "div"){
+    divide(ask , ask2)
+   }
+   if(ask0 == "by"){
+    muntiply(ask , ask2)
+   }
 }
